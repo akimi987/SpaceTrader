cdefa63b456d8401c30084bb9152528a3fc06a48
akimi987 <kadreinfo@gmail.com>
2024-01-12 23:02:07 +0100

    ....

--- spacetrader/src/pages/InfoVaissaux.jsx
+++ spacetrader/src/pages/InfoVaissaux.jsx
@@ -254,260 +254,260 @@ export default function Vaisseau(props = {}) {
   }
 
   return (
-    <> 
-  <Header />
-  <div className="custom-page">
-  <section className="custom-vaisseaux">
-    <Link className="custom-vaisseaux__retour" to={"/vaisseaux"}>
-      <svg
-        width="20"
-        height="18"
-        viewBox="0 0 20 18"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M1.25 9L8.75 1.5M1.25 9L8.75 16.5M1.25 9L18.75 9"
-          stroke="#FF3333"
-          strokeWidth="2.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>{" "}
-      Retour{" "}
-    </Link>
-    <div className="custom-vaisseau__page">
-      <div className="custom-vaisseau__page--left">
-        <div className="custom-vaisseau__page--name">
-          <h3 className="custom-h3__title">
-            {data ? data.symbol : "Chargement..."}
-          </h3>
-          <p>{data ? data.engine.name : "Chargement..."}</p>
-        </div>
-        <div className="custom-vaisseau__page--place">
-          <div>
-            <p className="custom-strong">
-              {data ? data.nav.route.destination.type : "Chargement..."} -{" "}
-              {data ? data.nav.route.destination.symbol : "Chargement..."}
-            </p>
-            <p>
-              {waypointInfo &&
-                waypointInfo.traits &&
-                waypointInfo.traits.map((trait, index) => (
-                  <span key={index}>
-                    {trait.name}
-                    {index !== waypointInfo.traits.length - 1 ? ", " : ""}
+    <>
+      <Header />
+      <div className="custom-page">
+        <section className="custom-vaisseaux">
+          <Link className="custom-vaisseaux__retour" to={"/vaisseaux"}>
+            <svg
+              width="20"
+              height="18"
+              viewBox="0 0 20 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1.25 9L8.75 1.5M1.25 9L8.75 16.5M1.25 9L18.75 9"
+                stroke="#FF3333"
+                strokeWidth="2.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>{" "}
+            Retour{" "}
+          </Link>
+          <div className="custom-vaisseau__page">
+            <div className="custom-vaisseau__page--left">
+              <div className="custom-vaisseau__page--name">
+                <h3 className="custom-h3__title">
+                  {data ? data.symbol : "Chargement..."}
+                </h3>
+                <p>{data ? data.engine.name : "Chargement..."}</p>
+              </div>
+              <div className="custom-vaisseau__page--place">
+                <div>
+                  <p className="custom-strong">
+                    {data ? data.nav.route.destination.type : "Chargement..."} -{" "}
+                    {data ? data.nav.route.destination.symbol : "Chargement..."}
+                  </p>
+                  <p>
+                    {waypointInfo &&
+                      waypointInfo.traits &&
+                      waypointInfo.traits.map((trait, index) => (
+                        <span key={index}>
+                          {trait.name}
+                          {index !== waypointInfo.traits.length - 1 ? ", " : ""}
+                        </span>
+                      ))}
+                  </p>
+                </div>
+              </div>
+              <div className="custom-vaisseau__page--flight">
+                <p>{data ? data.nav.flightMode : "Chargement..."}</p>
+                <select
+                  className="custom-btn__able"
+                  value={selectedMode}
+                  onChange={handleChange}
+                >
+                  <option value="changer" disabled>
+                    Choisir
+                  </option>
+                  <option value="CRUISE">CRUISE</option>
+                  <option value="STEALTH">STEALTH</option>
+                  <option value="DRIFT">DRIFT</option>
+                  <option value="BURN">BURN</option>
+                </select>
+              </div>
+              <div className="custom-vaisseau__page--status">
+                <p>{data ? data.nav.status : "Chargement..."} </p>
+                {data && data.nav.status === "IN_TRANSIT" && (
+                  <span>
+                    {data
+                      ? TimeFormat(data.nav.route.arrival)
+                      : "Calcul du temps restant..."}
                   </span>
-                ))}
-            </p>
-          </div>
-        </div>
-        <div className="custom-vaisseau__page--flight">
-          <p>{data ? data.nav.flightMode : "Chargement..."}</p>
-          <select
-            className="custom-btn__able"
-            value={selectedMode}
-            onChange={handleChange}
-          >
-            <option value="changer" disabled>
-              Choisir
-            </option>
-            <option value="CRUISE">CRUISE</option>
-            <option value="STEALTH">STEALTH</option>
-            <option value="DRIFT">DRIFT</option>
-            <option value="BURN">BURN</option>
-          </select>
-        </div>
-        <div className="custom-vaisseau__page--status">
-          <p>{data ? data.nav.status : "Chargement..."} </p>
-          {data && data.nav.status === "IN_TRANSIT" && (
-            <span>
-              {data
-                ? TimeFormat(data.nav.route.arrival)
-                : "Calcul du temps restant..."}
-            </span>
-          )}
-          {data && data.nav.status === "IN_ORBIT" && (
-            <button className="custom-btn__able" onClick={handleLanding}>
-              Atterir
-            </button>
-          )}
-          {data && data.nav.status === "DOCKED" && (
-            <button className="custom-btn__able" onClick={handleOrbit}>
-              Décoller
-            </button>
-          )}
-        </div>
-        {data ? (
-          data.nav.status === "IN_TRANSIT" ? (
-            <div className="custom-btn__disabled">Marketplace</div>
-          ) : (
-            <button onClick={showMarketData} className="custom-btn__able">
-              Marketplace
-            </button>
-          )
-        ) : (
-          <div>Chargement en cours...</div>
-        )}
-        <div className="custom-vaisseau__waypoints">
-          {data ? (
-            data.nav.status === "IN_ORBIT" ? (
-              <>
-                {data.mounts.some((mount) =>
-                  mount.name.toLowerCase().includes("sensor")
-                ) ? (
-                  <button
-                    className="custom-btn__able"
-                    onClick={handleScanWaypoints}
-                  >
-                    Scannez les Waypoints
-                  </button>
-                ) : (
-                  <>
-                    <button disabled className="custom-btn__disabled">
-                      Scannez les Waypoints
-                    </button>
-                  </>
                 )}
-              </>
-            ) : (
-              <>
-                <button disabled className="custom-btn__disabled">
-                  Décoller pour scannez les Waypoints
-                </button>
-              </>
-            )
-          ) : null}
-        </div>
-        <div className="custom-vaisseau__minerais">
-          {data ? (
-            data.nav.status === "IN_ORBIT" &&
-            [
-              "ENGINEERED_ASTEROID",
-              "ASTEROID",
-              "ASTEROID_FIELD",
-            ].includes(data.nav.route.destination.type) ? (
-              <button
-                className="custom-btn__able"
-                onClick={handleExtractResources}
-              >
-                Extraire du minerais
-              </button>
-            ) : (
-              <>
-                <button disabled className="custom-btn__disabled">
-                  Extraire du minerais
-                </button>
-              </>
-            )
-          ) : null}
-        </div>
-      </div>
-      <div className="custom-vaisseau__page--right">
-        <div className="custom-vaisseau__page--contain"></div>
-      </div>
-    </div>
-    {marketData ? (
-      <section className="custom-marketplace hidden">
-        <Link className="custom-vaisseaux__retour" to={"/vaisseaux"}>
-          <svg
-            width="20"
-            height="18"
-            viewBox="0 0 20 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1.25 9L8.75 1.5M1.25 9L8.75 16.5M1.25 9L18.75 9"
-              stroke="#FF3333"
-              strokeWidth="2.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>{" "}
-          Retour{" "}
-        </Link>
-        <h2> Produits disponibles</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Symbol</th>
-              <th>Type</th>
-              <th>Prix d'achat</th>
-              <th>Prix de vente</th>
-            </tr>
-          </thead>
-          <tbody>
-            {marketData.tradeGoods && marketData.tradeGoods.length > 0 ? (
-              marketData.tradeGoods.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.symbol}</td>
-                  <td>{item.type}</td>
-                  <td>{item.purchasePrice}</td>
-                  <td>{item.sellPrice}</td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan="4">Loading...</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </section>
-    ) : (
-      <section className="hidden"></section>
-    )}
-    {apiResponse ? (
-      <section className="custom-listwaypoints">
-        <button className="custom-closed" onClick={LocationReload}></button>
-        <h2> Waypoints</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Objet</th>
-              <th>Distance</th>
-              <th>Caractéristiques</th>
-              <th>Type d'objet</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {apiResponse.waypoints.map((item, index) => (
-              <tr key={index}>
-                <td>{item.symbol}</td>
-                <td>
-                  <DistanceDisplay
-                    x1={item.x}
-                    y1={item.y}
-                    x2={data.nav.route.destination.x}
-                    y2={data.nav.route.destination.y}
-                  />
-                </td>
-                <td>
-                  {item.traits.map((trait, traitIndex) => (
-                    <p key={traitIndex}>{trait.name}</p>
-                  ))}
-                </td>
-                <td>{item.type}</td>
-                <td>
-                  <button
-                    className="custom-btn__able"
-                    onClick={() => handleGoClick(item.symbol)}
-                  >
-                    Explorer
+                {data && data.nav.status === "IN_ORBIT" && (
+                  <button className="custom-btn__able" onClick={handleLanding}>
+                    Atterir
                   </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </section>
-    ) : (
-      <section className="custom-listwaypoints hidden"></section>
-    )}
-  </section>
-</div> 
+                )}
+                {data && data.nav.status === "DOCKED" && (
+                  <button className="custom-btn__able" onClick={handleOrbit}>
+                    Décoller
+                  </button>
+                )}
+              </div>
+              {data ? (
+                data.nav.status === "IN_TRANSIT" ? (
+                  <div className="custom-btn__disabled">Marketplace</div>
+                ) : (
+                  <button onClick={showMarketData} className="custom-btn__able">
+                    Marketplace
+                  </button>
+                )
+              ) : (
+                <div>Chargement en cours...</div>
+              )}
+              <div className="custom-vaisseau__waypoints">
+                {data ? (
+                  data.nav.status === "IN_ORBIT" ? (
+                    <>
+                      {data.mounts.some((mount) =>
+                        mount.name.toLowerCase().includes("sensor")
+                      ) ? (
+                        <button
+                          className="custom-btn__able"
+                          onClick={handleScanWaypoints}
+                        >
+                          Scannez les Waypoints
+                        </button>
+                      ) : (
+                        <>
+                          <button disabled className="custom-btn__disabled">
+                            Scannez les Waypoints
+                          </button>
+                        </>
+                      )}
+                    </>
+                  ) : (
+                    <>
+                      <button disabled className="custom-btn__disabled">
+                        Décoller pour scannez les Waypoints
+                      </button>
+                    </>
+                  )
+                ) : null}
+              </div>
+              <div className="custom-vaisseau__minerais">
+                {data ? (
+                  data.nav.status === "IN_ORBIT" &&
+                    [
+                      "ENGINEERED_ASTEROID",
+                      "ASTEROID",
+                      "ASTEROID_FIELD",
+                    ].includes(data.nav.route.destination.type) ? (
+                    <button
+                      className="custom-btn__able"
+                      onClick={handleExtractResources}
+                    >
+                      Extraire du minerais
+                    </button>
+                  ) : (
+                    <>
+                      <button disabled className="custom-btn__disabled">
+                        Extraire du minerais
+                      </button>
+                    </>
+                  )
+                ) : null}
+              </div>
+            </div>
+            <div className="custom-vaisseau__page--right">
+              <div className="custom-vaisseau__page--contain"></div>
+            </div>
+          </div>
+          {marketData ? (
+            <section className="custom-marketplace hidden">
+              <Link className="custom-vaisseaux__retour" to={"/vaisseaux"}>
+                <svg
+                  width="20"
+                  height="18"
+                  viewBox="0 0 20 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1.25 9L8.75 1.5M1.25 9L8.75 16.5M1.25 9L18.75 9"
+                    stroke="#FF3333"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>{" "}
+                Retour{" "}
+              </Link>
+              <h2> Produits disponibles</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Symbol</th>
+                    <th>Type</th>
+                    <th>Prix d'achat</th>
+                    <th>Prix de vente</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {marketData.tradeGoods && marketData.tradeGoods.length > 0 ? (
+                    marketData.tradeGoods.map((item, index) => (
+                      <tr key={index}>
+                        <td>{item.symbol}</td>
+                        <td>{item.type}</td>
+                        <td>{item.purchasePrice}</td>
+                        <td>{item.sellPrice}</td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="4">Loading...</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </section>
+          ) : (
+            <section className="hidden"></section>
+          )}
+          {apiResponse ? (
+            <section className="custom-listwaypoints">
+              <button className="custom-closed" onClick={LocationReload}></button>
+              <h2> Waypoints</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Objet</th>
+                    <th>Distance</th>
+                    <th>Caractéristiques</th>
+                    <th>Type d'objet</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {apiResponse.waypoints.map((item, index) => (
+                    <tr key={index}>
+                      <td>{item.symbol}</td>
+                      <td>
+                        <DistanceDisplay
+                          x1={item.x}
+                          y1={item.y}
+                          x2={data.nav.route.destination.x}
+                          y2={data.nav.route.destination.y}
+                        />
+                      </td>
+                      <td>
+                        {item.traits.map((trait, traitIndex) => (
+                          <p key={traitIndex}>{trait.name}</p>
+                        ))}
+                      </td>
+                      <td>{item.type}</td>
+                      <td>
+                        <button
+                          className="custom-btn__able"
+                          onClick={() => handleGoClick(item.symbol)}
+                        >
+                          Explorer
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </section>
+          ) : (
+            <section className="custom-listwaypoints hidden"></section>
+          )}
+        </section>
+      </div>
     </>
   );
 }
--- spacetrader/src/pages/Vaissaux.jsx
+++ spacetrader/src/pages/Vaissaux.jsx
@@ -1,3 +1,4 @@
+/* Said */
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
